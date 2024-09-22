6dcafea340d4ac83296003a31c37ddcb9bd66c77
ArtherCasan <ArtherCasan@outlook.com>
2024-09-22 18:06:56 +0800

    2.7.0 NEI Update

--- src/main/java/net/moecraft/nechar/NecharSearchProvider.java
+++ src/main/java/net/moecraft/nechar/NecharSearchProvider.java
@@ -1,17 +1,30 @@
 package net.moecraft.nechar;
 
-import codechicken.nei.SearchField;
+import net.minecraft.util.EnumChatFormatting;
+
+import codechicken.nei.SearchTokenParser.ISearchParserProvider;
+import codechicken.nei.SearchTokenParser.SearchMode;
 import codechicken.nei.api.ItemFilter;
 
-public class NecharSearchProvider implements SearchField.ISearchProvider {
-
-    @Override
-    public boolean isPrimary() {
-        return false;
-    }
+public class NecharSearchProvider implements ISearchParserProvider {
 
     @Override
     public ItemFilter getFilter(String searchText) {
         return new NecharSearchFilter(searchText);
+    }
+
+    @Override
+    public char getPrefix() {
+        return '*';
+    }
+
+    @Override
+    public EnumChatFormatting getHighlightedColor() {
+        return EnumChatFormatting.BLUE;
+    }
+
+    @Override
+    public SearchMode getSearchMode() {
+        return SearchMode.ALWAYS;
     }
 }
