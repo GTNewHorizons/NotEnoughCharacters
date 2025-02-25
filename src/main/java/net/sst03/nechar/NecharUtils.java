eb0dddb7c19038e6cdf23f6061f3b402dd59852d
Sam0324 <Sam0324@qq.com>
2025-02-25 22:45:35 +0800

    2

--- src/main/java/net/sst03/nechar/NecharUtils.java
+++ src/main/java/net/sst03/nechar/NecharUtils.java
@@ -4,17 +4,17 @@ import static net.moecraft.nechar.NotEnoughCharacters.CONTEXT;
 
 public class NecharUtils {
 
-    private String deleteComma(String str) {
+    private static String deleteComma(String str) {
         return str.replaceAll("(?<=[0-9]),(?=[0-9])", "");
     }
 
-    private String deleteUnlegalChars(String str) {
+    private static String deleteUnlegalChars(String str) {
         return str.replaceAll("\ud872","").replaceAll("\ud86d","");
     }
 
     public static String contain(String searchText,String sourseText) {
 
-        if (!(searchText.contains("\ud872") || !searchText.contains("\ud86d"))) {
+        if (!(searchText.contains("\ud872") || searchText.contains("\ud86d"))) {
             searchText = deleteUnlegalChars(searchText);
             sourseText = deleteUnlegalChars(sourseText);
         }
